760f2c050c69f4b28cd95c7fee8288c16c5d259f
Abe <43585616+AbeNalee@users.noreply.github.com>
2022-08-27 22:56:55 +0300

    game client

---
+++ src/connection-manager.js
@@ -0,0 +1,50 @@
+import io from 'socket.io-client'
+
+export default class ConnectionManager {
+  constructor () {
+    this.socket = null
+    this.initSocket()
+  }
+
+  initSocket () {
+    if (!this.socket) {
+      if (window.location.hostname === 'localhost') {
+        this.socket = io('http://localhost:8081')
+      } else {
+        this.socket = io('https://spyfall-server.onrender.com')
+      }
+    } else {
+      this.socket.connect()
+    }
+  }
+
+  connect (playerName, sessionId, wager, numPlayers, rounds, connectionClosedCallback, onMessageCallback) {
+    this.initSocket()
+    this.send('join-session', {
+      sessionId: sessionId,
+      playerName: playerName,
+      wager: wager,
+      numPlayers: numPlayers,
+      rounds: rounds,
+      game: 'spy'
+    })
+
+    this.socket.on('disconnect', () => {
+      this.socket = null
+      connectionClosedCallback()
+    })
+
+    this.socket.on('message', (msg) => {
+      onMessageCallback(msg.type, msg)
+    })
+  }
+
+  disconnect () {
+    this.socket.disconnect()
+    this.socket = null
+  }
+
+  send (type, data) {
+    this.socket.emit(type, data)
+  }
+}
